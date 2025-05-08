9cc0f9470b186152ddd3d893e13614fb7a1ad878
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-08 14:59:32 +0000

    Update 5 Assign-03

--- result.php
+++ result.php
@@ -2,13 +2,14 @@
 $age = $_POST['Age'];
 $day = $_POST['day'];
 
-if ($age <= 5 || $age >= 95) {
-    echo "Entry is free";
-} else if ($age >= 12 && $age <= 21) {
+
+    if ($age >= 12 && $age <= 21) {
     echo "You get a discount";
-} else if ($day == "Tuesday" || $day == "Thursday") {
+} 
+else if ($day == "Tuesday" || $day == "Thursday") {
     echo "You get a discount";
-} else {
+}
+ else {
     echo "Pay regular price";
 }
 ?>
