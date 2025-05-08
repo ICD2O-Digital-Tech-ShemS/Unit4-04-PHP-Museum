dcf0f2eee2d23b161cd50b37c06474d2806eba83
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-08 14:55:58 +0000

    Update 5 Assign-0

--- result.php
+++ result.php
@@ -6,7 +6,7 @@ if ($age <= 5 || $age >= 95) {
     echo "Entry is free";
 } else if ($age >= 12 && $age <= 21) {
     echo "You get a discount";
-} else if ($day === "Tuesday" || $day === "Thursday") {
+} else if ($day == "Tuesday" || $day == "Thursday") {
     echo "You get a discount";
 } else {
     echo "Pay regular price";
