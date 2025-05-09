5f7ccc094ac984eedfcff95017779a8e44cf8796
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-09 15:29:38 +0000

    This is my project

--- result.php
+++ result.php
@@ -4,11 +4,9 @@ $day = $_POST['day'];
 
 // Proper syntax: entire condition inside one set of parentheses
 if (($age <= 5) && ($age >= 95)) {
-    echo "You get a discount";
-}else if ($day == "Tuesday") || ($day == "Thursday") {
-    echo "You get a discount";
+    echo "It is free";
 }
- else {
+    else {
     echo "Pay regular price";
 }
 ?>
